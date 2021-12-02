0fe1d79af608e6e9f8dae8f1598d60ff376cd4ab
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-02 12:17:37 +0100

    Check name

---
+++ snack-2/index.php
@@ -0,0 +1,30 @@
+<?php
+    /* Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+
+    $name = $_GET["name"];
+    #strlen($name);
+    $nameLen = strlen($name);
+    if ($nameLen < 3) {
+        $name = 'Nome non valido';
+    }
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+    <p>nome: <?php echo $name;?></p>
+    
+</form>     
+</body>
+</html>
