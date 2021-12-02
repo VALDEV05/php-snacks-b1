1304058e3256b5f450b0e7900b6833d2f48074d4
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-02 11:56:43 +0100

    Complete exercise with for loop

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,34 +1,42 @@
 <?php
     /* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
     Olimpia Milano - Cantù | 55-60 */
-        $prima_partita=[
-            $andata=[
+        $prima_partita = [
+            [
                 'casa'=> 'Milano',
                 'ospite'=> 'Cantù', 
                 'punti_casa'=>'80', 
-                'punti_ospite'=> '60'],
-            $ritorno=[
+                'punti_ospite'=> '60'
+            ],
+            [
                 'casa'=> 'Cantù',
                 'ospite'=> 'Milano', 
                 'punti_casa'=>'109', 
-                'punti_ospite'=> '70']];
+                'punti_ospite'=> '70'
+                ]
+            ];
         
-    ?>
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>
+    <body>
+            <h1>PARTITE MILANO CANTÙ</h1>
+        <div>
+            <?php for ($i = 0; $i < count($prima_partita); $i++) {
+            $game = $prima_partita[$i]; ?>
+            <p><?= $game['casa'] ?> - <?= $game['ospite'] ?> | <?= $game['punti_casa'] ?>-<?= $game['punti_ospite'] ?></p>
+        <?php } ?>
+        </div>
 
-<h1>Giornata 1 Milano-Cantù</h1>
-<h2>Andata</h2>
-  <p><?= $andata['casa'].' - '.$andata[ospite].' | '.$andata['punti_casa'].' - '.$andata['punti_ospite']?></p>
-  <h2>Ritorno</h2>
-    <p><?= $ritorno['casa'].' - '.$ritorno[ospite].' | '.$ritorno['punti_casa'].' - '.$ritorno['punti_ospite']?></p>
-
-</body>
+    </body>
 </html>
+    
+
