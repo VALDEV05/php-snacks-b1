0afaf603e9419c89093aa55fc0a7bf973a30afad
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-03 12:51:58 +0100

    complete snack-2

--- Blocco-2/snack-2/data.php
+++ Blocco-2/snack-2/data.php
@@ -1,4 +1,20 @@
 <?php
     /* Creiamo due pagine php. La prima farà una chiamata GET alla seconda attraverso un form. Il form avrà un input di testo in cui inserire il nome di un invitato. Inseriamo nell’altro file una lista di invitati. Se la nostra query è contenuta nella lista degli invitati stampare ok, altrimenti ko. */
-    var_dump($_GET);
+
+    
+    /* var_dump($_GET); */
+    
+    $items=[
+        'valerio',
+        'daniele',
+        'giuseppe',
+        'valentina',
+        'fabio'
+    ];
+
+    if (in_array($_GET['text'], $items)) {
+        echo 'presente in lista';
+    }else{
+        echo 'non è presente in lista';
+    }
 ?>
--- Blocco-2/snack-2/form.php
+++ Blocco-2/snack-2/form.php
@@ -9,10 +9,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>form.php</title>
 </head>
 <body>
-    <form action="data.php" method="get">
+    <form action="./data.php" method="get">
        Text to check: <input type="text" name="text"><br>
         <input type="submit">
     </form>
