d6e81da8a861203ddb9ba67f6c2891b7e7252ed3
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-03 12:52:55 +0100

    completed as requested

--- Blocco-2/snack-2/data.php
+++ Blocco-2/snack-2/data.php
@@ -1,7 +1,7 @@
 <?php
     /* Creiamo due pagine php. La prima farà una chiamata GET alla seconda attraverso un form. Il form avrà un input di testo in cui inserire il nome di un invitato. Inseriamo nell’altro file una lista di invitati. Se la nostra query è contenuta nella lista degli invitati stampare ok, altrimenti ko. */
 
-    
+
     /* var_dump($_GET); */
     
     $items=[
@@ -13,8 +13,10 @@
     ];
 
     if (in_array($_GET['text'], $items)) {
-        echo 'presente in lista';
+        echo 'OK';
+
     }else{
-        echo 'non è presente in lista';
+        echo 'KO';
+        
     }
 ?>
