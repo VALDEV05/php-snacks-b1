57c22e11c6ef4f8b468dc8946f734751f473ea2b
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-03 12:41:04 +0100

    my idea is more or less this

---
+++ Blocco-2/snack-2/data.php
@@ -0,0 +1,4 @@
+<?php
+    /* Creiamo due pagine php. La prima farà una chiamata GET alla seconda attraverso un form. Il form avrà un input di testo in cui inserire il nome di un invitato. Inseriamo nell’altro file una lista di invitati. Se la nostra query è contenuta nella lista degli invitati stampare ok, altrimenti ko. */
+    var_dump($_GET);
+?>
---
+++ Blocco-2/snack-2/form.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="data.php" method="get">
+       Text to check: <input type="text" name="text"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
